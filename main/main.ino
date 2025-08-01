0531aac8af6a87506b7ccc285466fa27aaea8269
Masafumi Horimoto on badman <horimoto.masafumi.470@s.kyushu-u.ac.jp>
2025-08-01 21:30:25 +0900

    2025年OpenCampus仕様

--- main/main.ino
+++ main/main.ino
@@ -100,39 +100,39 @@ void loop(void) {
       lcd.clear();
     }
   }
-  a1 = analogRead(A0);
+  a1 = analogRead(A2);
   li = map(a1,0,1022,-100,500);
   a1b = (int)(li/10);
   a1c = (int)(li-(a1b*10));
   a2 = analogRead(A1);
   li = map(a2,0,1022,0,100);
   a2 = (int)li;
-  a3 = analogRead(A2);
+  a3 = analogRead(A0);
   li = map(a3,0,1023,0,1300);
   a3 = (int)li;
   a4 = analogRead(A3);
-  li = map(a4,0,1023,200,2000);
+  li = map(a4,0,1023,0,100);
   a4 = (int)li;
   
   sprintf(s1,"%3d.%01d",a1b,abs(a1c));  // Temp
   sprintf(s2,"%3d",a2);                 // Humi
   sprintf(s3,"%4d",a3);                 // Radiation
-  sprintf(s4,"%4d",a4);                 // CO2
+  sprintf(s4,"%4d",a4);                 // SoilWater
   if (lcdf==1) {
     lcd.setCursor(0,1);
-    lcd.print("T:");
-    lcd.setCursor(0,0);
     lcd.print("R:");
+    lcd.setCursor(0,0);
+    lcd.print("T:");
     lcd.setCursor(10,0);
-    lcd.print("C:");
-    lcd.setCursor(10,1);
     lcd.print("H:");
+    lcd.setCursor(10,1);
+    lcd.print("S:");
     lcd.setCursor(2,1);
-    lcd.print(s1);
+    lcd.print(s3);
     lcd.setCursor(13,1);
     lcd.print(s2);
     lcd.setCursor(2,0);
-    lcd.print(s3);
+    lcd.print(s1);
     lcd.setCursor(12,0);
     lcd.print(s4);
   } else {
